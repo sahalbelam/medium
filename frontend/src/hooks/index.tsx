6e76678f89582cfdd18307d86d824fd2b644d185
SahalBelam <belamsahal8@gmail.com>
2024-09-25 14:41:57 +0530

    added config

--- frontend/src/hooks/index.tsx
+++ frontend/src/hooks/index.tsx
@@ -11,7 +11,7 @@ export interface Blog {
     }
 }
 
-export const useBlogs = (p0?: { id: string }) => {
+export const useBlogs = (_p0?: { id: string }) => {
     const [loading, setLoading] = useState(true)
     const [blogs, setBlogs] = useState<Blog[]>([])
 
